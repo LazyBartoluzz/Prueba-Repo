aadd917a3201a71b6688fe724f91e3d594b83e24
Joan Torres <rostysalckgilman@gmail.com>
2025-02-19 22:02:06 -0500

    archivo plano con algo de texto

---
+++ AlgoritmosUtiles/src/demo/EjercicioDeSimetria.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class EjercicioDeSimetria {
+
+	public static void main(String[] args) {
+		
+		System.out.println("la idea es escribir un programa capaz de saber si una "
+				+ "imágen con patrones presentada para un concurso,"
+				+ "es valida o si por el contrario no lo es");
+		System.out.println();
+		System.out.println("El ejercicio es de la autoria de la siguiente pagina");
+		System.out.println( "https://aceptaelreto.com/problem/statement.php?id=530" );
+		System.out.println("Para más detalles :v");
+		
+	}
+
+}
